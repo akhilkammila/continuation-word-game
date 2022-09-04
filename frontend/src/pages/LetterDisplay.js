9e082917803ba966b2f66ac59e41e8c5658cdccb
Akhil Kammila <akhilkammila@gmail.com>
2022-09-04 12:00:32 -0400

    changed first letter in game word to black when in light mode

--- frontend/src/pages/LetterDisplay.js
+++ frontend/src/pages/LetterDisplay.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react'
 import {Box, Heading, Button, Image, Flex, Text} from "@chakra-ui/react"
+import {IconButton, useColorMode, useColorModeValue} from '@chakra-ui/react'
 
 const LetterDisplay = ({letter, index, numPlayers}) => {
     const colorList = ["green", "red.600", "blue.600", "orange.600", "yellow.600", "purple.600"]
-    let color="white"
+    let color = useColorModeValue('black', 'white')
     if(index>0){
         console.log(numPlayers)
         color = colorList[(index-1)%numPlayers]
