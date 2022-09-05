c83341529ca42db5fa97b0e786cab95850c83b2e
Akhil Kammila <akhilkammila@gmail.com>
2022-09-05 18:24:20 -0400

    changed cors origin to any to stop socket.io blocking from vercel

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "localhost:3000",
+        origin: "*",
         methods: ["GET", "POST"],
     }
 })
